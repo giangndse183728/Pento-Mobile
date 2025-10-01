154e2834b537c0bf1d362da923ab33c9f50d299b
giangndse183728 <giangndse183728@fpt.edu.vn>
2025-10-01 18:39:10 +0700

    chore(ui): add color constants & icon

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color iceberg = Color(0xFFF7FBFC); // #F7FBFC
+  static const Color babyBlue = Color(0xFFD6E6F2); // #D6E6F2
+  static const Color powderBlue = Color(0xFFB9D7EA); // #B9D7EA
+  static const Color blueGray = Color(0xFF769FCD); // #769FCD (accent)
+}
+
+
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,11 @@
+class AppImages {
+  const AppImages._();
+
+  // Bottom navigation icons (place PNG files under assets/image/)
+  static const String pantry = 'assets/image/pantry.png';
+  static const String meal = 'assets/image/meal.png';
+  static const String posts = 'assets/image/post.png';
+  static const String cart = 'assets/image/cart.png';
+}
+
+
