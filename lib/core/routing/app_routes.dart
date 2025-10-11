f5c617538758d2d55b18f8601911238d7fbaa646
giangndse183728 <giangndse183728@fpt.edu.vn>
2025-10-11 19:43:57 +0700

    feat(router): setup app routing

---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,12 @@
+class AppRoutes {
+  const AppRoutes._();
+
+  static const String main = '/main';
+  static const String pantry = '/pantry';
+  static const String meal = '/meal';
+  static const String posts = '/posts';
+  static const String cart = '/cart';
+  static const String barcode = '/barcode';
+  static const String onboarding = '/onboarding';
+  static const String auth = '/auth';
+}
