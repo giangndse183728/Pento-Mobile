679922431012928cc67729f179748655f38b6788
giangndse183728 <giangndse183728@fpt.edu.vn>
2025-11-30 20:50:03 +0700

    chore: update color scheme and image

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -7,6 +7,9 @@ class AppColors {
   static const Color babyBlue = Color(0xFFD6E6F2); // #D6E6F2
   static const Color powderBlue = Color(0xFFB9D7EA); // #B9D7EA
   static const Color blueGray = Color(0xFF769FCD); // #769FCD (accent)
+  static const Color mintLeaf = Color(0xFFA8E6CF); // #A8E6CF
+  static const Color warningSun = Color(0xFFFFC857); // #FFC857
+  static const Color dangerRed = Color(0xFFFF6B6B); // #FF6B6B
 }
 
 
