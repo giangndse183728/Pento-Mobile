9926dd5b1894ffbbbb445674dd0d3b66d0b7b1c1
giangndse183728 <giangndse183728@fpt.edu.vn>
2025-12-03 01:47:59 +0700

    feat(notification): config android for notification

--- android/app/src/main/kotlin/com/example/pento_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pento_mobile/MainActivity.kt
@@ -1,5 +1,35 @@
 package com.example.pento_mobile
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.os.Bundle
+import android.content.Context
+import android.app.Notification
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    createHighImportanceNotificationChannel()
+  }
+
+  private fun createHighImportanceNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      val channelId = "high_importance_channel"
+      val name = "High importance notifications"
+      val descriptionText = "Used for heads-up notifications."
+      val importance = NotificationManager.IMPORTANCE_HIGH
+      val channel = NotificationChannel(channelId, name, importance).apply {
+        description = descriptionText
+        enableVibration(true)
+        setShowBadge(true)
+        lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+      }
+
+      val notificationManager =
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+      notificationManager.createNotificationChannel(channel)
+    }
+  }
+}
